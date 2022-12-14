92c393483da424d22c878f701bc25cce5f46714d
Dmitriy <dima2394@mail.ru>
2022-12-14 13:36:15 +0300

    Initial

---
+++ firstLesson/firstLesson/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  firstLesson
+//
+//  Created by Dmitriy Verennik on 14.12.2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ firstLesson/firstLesson/firstLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstLessonApp.swift
+//  firstLesson
+//
+//  Created by Dmitriy Verennik on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct firstLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
